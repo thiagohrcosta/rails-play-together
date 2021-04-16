3ff3e3dc615e2a76de8837b4cefc3a12e178805c
Thiago Costa <thiagohrcosta86@gmail.com>
2021-04-16 10:37:54 -0300

    Creating the Show Page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,23 @@ class PostsController < ApplicationController
   def show
     @posts = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :username, :game, :cover, :content)
+  end
 end
